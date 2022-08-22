ad179bfb93b58a59cdfe6c3ea2b2ce9ca527c1c5
Evans <evans.njeru@student.moringaschool.com>
2022-08-22 08:37:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 62, num2 = 1;
+let multiply = num1 * num2; 
 
-// Write your code here
+let random = Math.round((Math.random() * 10) + 1);
+
+let num3 = 24, num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
